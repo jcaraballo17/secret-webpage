86d93ac573ffda98c7070bc2bfac3612389484a2
Juan Caraballo <juan.caraballo17@gmail.com>
2015-07-31 01:22:18 -0600

    Added models for the Home Page Images, the Announcements, and the Pieces.

--- paintings/models.py
+++ paintings/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class HomePageImage(models.Model):
+    name = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='home_images')
+
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=256)
+    date = models.DateField(auto_now_add=True)
+    description = models.TextField()
+    short_description = models.CharField(max_length=1024)
+    active = models.BooleanField(default=True)
+
+
+class Piece(models.Model):
+    title = models.CharField(max_length=256)
+    date = models.DateField()
+    description = models.CharField(max_length=512)
+    size = models.CharField(max_length=32)
+    medium = models.CharField(max_length=128)

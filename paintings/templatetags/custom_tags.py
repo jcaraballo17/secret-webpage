4bf5241a1e1e30e6246b650cbc285a8858202fa6
Juan Caraballo <juan.caraballo17@gmail.com>
2016-03-11 13:34:46 -0700

    Added new custom tag to get embed link from youtube video url.

--- paintings/templatetags/custom_tags.py
+++ paintings/templatetags/custom_tags.py
@@ -1,8 +1,19 @@
 from urlparse import parse_qs
 from django import template
-from django.template.defaultfilters import stringfilter
 
 register = template.Library()
+
+
+@register.filter('embed')
+def youtube_embed(url):
+    """
+    :param url: string containing youtube video url.
+    :return: a string containing the youtube video embed url.
+    """
+    qs = url.split('?')
+    video_id = parse_qs(qs[1])['v'][0]
+
+    return "https://www.youtube.com/embed/%s?rel=0" % video_id
 
 
 @register.filter('youthumbnail')

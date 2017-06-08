0bbcd19bba54de80527a8395b50a3c934d6b5c45
Juan Caraballo <juan.caraballo17@gmail.com>
2017-06-07 21:19:38 -0600

    Updated production server settings file.

--- woodypage/settings/production.py
+++ woodypage/settings/production.py
@@ -1,1 +1,16 @@
 __author__ = 'jcaraballo17'
+
+from woodypage.settings.base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', data["host"]]
+
+STATIC_ROOT = data["static_root"]
+STATIC_URL = "/static/"
+
+SITE_ROOT = "/opt/woodypage/"
+SITE_URL = ""
+
+MEDIA_ROOT = data["media_root"]
+MEDIA_URL = "/media/"

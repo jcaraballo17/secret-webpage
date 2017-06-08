27f3bc1a15f9569523bb28caabe7cf2b1329ca06
Juan Caraballo <juan.caraballo17@gmail.com>
2017-06-07 21:20:54 -0600

    Added seo keywords by models and individual pages.

--- paintings/templatetags/custom_tags.py
+++ paintings/templatetags/custom_tags.py
@@ -1,3 +1,19 @@
+import re
 from django import template
 
 register = template.Library()
+
+common_words = ['a', 'the', 'is', 'this', 'in', 'on', 'all']
+
+
+@register.filter('keywords')
+def generate_keywords(instance):
+    """
+    :param instance: model instance to get the keywords from.
+    :return: a string containing all the keywords for this instance.
+    """
+
+    instance_title = instance.__str__().lower()
+    reduced_title = filter(lambda word: (word not in common_words and word != ''), re.split(r'\W+', instance_title))
+    keywords = ', '.join(str(keyword) for keyword in reduced_title)
+    return keywords
